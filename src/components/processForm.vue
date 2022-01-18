18a8d2356f0f1c62c4c254b5630855b26f0f7917
Oleksandr Zoria <oleksandr.zoria@p2h.com>
2022-01-18 16:52:00 +0200

    fix

--- src/components/processForm.vue
+++ src/components/processForm.vue
@@ -2,24 +2,24 @@
   <form class="process-form">
     <h3 class="h3">{{ survey.Header }}</h3>
     <div class="list-holder">
-      <input class="checkbox-tools" type="radio" name="tools" :id="survey.label.one">
-      <label class="for-checkbox-tools" :for="survey.label.one">
+      <input v-if="survey.label && survey.label.one" class="checkbox-tools" type="radio" name="tools" :id="survey.label.one">
+      <label v-if="survey.label && survey.label.one" class="for-checkbox-tools" :for="survey.label.one">
         {{ survey.mark.one }}
       </label>
-      <input class="checkbox-tools" type="radio" name="tools" :id="survey.label.two">
-      <label class="for-checkbox-tools" :for="survey.label.two">
+      <input v-if="survey.label && survey.label.two" class="checkbox-tools" type="radio" name="tools" :id="survey.label.two">
+      <label v-if="survey.label && survey.label.two" class="for-checkbox-tools" :for="survey.label.two">
         {{ survey.mark.two }}
       </label>
-      <input class="checkbox-tools" type="radio" name="tools" :id="survey.label.three">
-      <label class="for-checkbox-tools" :for="survey.label.three">
+      <input v-if="survey.label && survey.label.three" class="checkbox-tools" type="radio" name="tools" :id="survey.label.three">
+      <label v-if="survey.label && survey.label.three" class="for-checkbox-tools" :for="survey.label.three">
         {{ survey.mark.three }}
       </label>
-      <input class="checkbox-tools" type="radio" name="tools" :id="survey.label.four">
-      <label class="for-checkbox-tools" :for="survey.label.four">
+      <input v-if="survey.label && survey.label.four" class="checkbox-tools" type="radio" name="tools" :id="survey.label.four">
+      <label v-if="survey.label && survey.label.four" class="for-checkbox-tools" :for="survey.label.four">
         {{ survey.mark.four }}
       </label>
-      <input class="checkbox-tools" type="radio" name="tools" :id="survey.label.five">
-      <label class="for-checkbox-tools" :for="survey.label.five">
+      <input v-if="survey.label && survey.label.five" class="checkbox-tools" type="radio" name="tools" :id="survey.label.five">
+      <label v-if="survey.label && survey.label.five" class="for-checkbox-tools" :for="survey.label.five">
         {{ survey.mark.five }}
       </label>
     </div>
